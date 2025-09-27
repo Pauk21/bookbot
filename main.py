b0f2758de270d918788601160f71da505c2bbbae
Pauk <niko.paukovic15@gmail.com>
2025-09-27 19:00:37 +0200

    Finished

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import sys
 from stats import get_word_count
 from stats import get_character_count
 from stats import sort_dict
+
 
 def get_book_text(file_path):
     with open(file_path) as file:
@@ -9,7 +11,10 @@ def get_book_text(file_path):
 
 
 def main():
-    book = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book = sys.argv[1]
     book_text = get_book_text(book)
     book_word_count = get_word_count(book_text)
     book_character_count = get_character_count(book_text)
